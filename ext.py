0c606f96cd46e28f3bac70b3ada8d792fd5bdf7b
Morgan Metz <68765538+Morgandri1@users.noreply.github.com>
2022-06-02 12:40:06 -0400

    added files for v2

---
+++ ext.py
@@ -0,0 +1,8 @@
+initial_extentions = [
+    "cogs.commands.info",
+    "cogs.commands.fun",
+    "cogs.commands.chance",
+    "cogs.commands.mod",
+    "cogs.commands.tools",
+    "cogs.events.events"
+]
